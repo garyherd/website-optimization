dda5ef24792c485fe3384a8acf2e14d407283177
Gary <garyherd@yahoo.com>
2016-08-23 22:40:04 -0500

    refactor: update view/main.js
    Break up for loop on line 500 to eliminate multiple paints

--- views/js/main.js
+++ views/js/main.js
@@ -497,14 +497,30 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 // https://www.igvita.com/slides/2012/devtools-tips-and-tricks/jank-demo.html
 
 // Moves the sliding background pizzas based on scroll position
+// function updatePositions() {
+//   frame++;
+//   window.performance.mark("mark_start_frame");
+
+//   var items = document.querySelectorAll('.mover');
+//   for (var i = 0; i < items.length; i++) {
+//     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+//     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+//   }
+
+// improved version of updatePositions
+// I think this part is causing of lot of painting.
 function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  var phases = [];
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    phases[i] = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+  }
+
+  for (var i = 0; i < items.length; i++) {
+    items[i].style.left = items[i].basicLeft + 100 * phases[i] + 'px';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
